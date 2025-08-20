1728002ae75f0a8ab5a2d7912da4722aba23b927
Aliaksandr Bullo <aliaksandr_bullo@epam.com>
2025-08-20 10:26:29 -0700

    fix terraform format check

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "3.0.0"
+  source          = "terraform-aws-modules/ecr/aws"
+  version         = "3.0.0"
   repository_name = "${var.project_name}-ecr-${var.environment}"
 }
