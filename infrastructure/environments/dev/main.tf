15105e5e640b1b5e5746abc8dc37e7bb5bd96c47
Aliaksandr Bullo <aliaksandr_bullo@epam.com>
2025-08-20 10:33:52 -0700

    fix Error: creating ECR Lifecycle Policy (ecs-refarch-batch-processing-ecr-dev): operation error ECR: PutLifecyclePolicy, https response error StatusCode: 400, RequestID: 53644870-6402-4ccc-81de-717800a4dde3, InvalidParameterException: Invalid parameter at 'lifecyclePolicyText' failed to satisfy constraint: 'Member must have length greater than or equal to 100' ref: https://github.com/virtuz/ecs-refarch-batch-processing/actions/runs/17105717279/job/48513950827

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -2,4 +2,21 @@ module "ecr" {
   source          = "terraform-aws-modules/ecr/aws"
   version         = "3.0.0"
   repository_name = "${var.project_name}-ecr-${var.environment}"
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
